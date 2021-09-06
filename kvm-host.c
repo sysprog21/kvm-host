3e5dcfdcce8ce6acfd37a95135022d3f53a3962e
Jim Huang <jserv@biilabs.io>
2021-09-06 23:32:11 +0800

    Simplify the error message dumper

--- kvm-host.c
+++ kvm-host.c
@@ -8,7 +8,6 @@
 
 #include <errno.h>
 #include <fcntl.h>
-#include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,20 +24,16 @@ typedef struct {
     void *mem;
 } vm_t;
 
-static int throw_err(const char *fmt, ...)
+static int throw_err(const char *str)
 {
-    va_list args;
-    va_start(args, fmt);
-    vfprintf(stderr, fmt, args);
-    fprintf(stderr, ", errno: %d\n", errno);
-    va_end(args);
+    fprintf(stderr, "%s (errno=%d)\n", str, errno);
     return -1;
 }
 
-static int vm_init_regs(vm_t *g)
+static int vm_init_regs(vm_t *v)
 {
     struct kvm_sregs sregs;
-    if (ioctl(g->vcpu_fd, KVM_GET_SREGS, &sregs) < 0)
+    if (ioctl(v->vcpu_fd, KVM_GET_SREGS, &sregs) < 0)
         return throw_err("Failed to get registers");
 
 #define X(R) sregs.R.base = 0, sregs.R.limit = ~0, sregs.R.g = 1
@@ -49,30 +44,30 @@ static int vm_init_regs(vm_t *g)
     sregs.ss.db = 1;
     sregs.cr0 |= 1; /* enable protected mode */
 
-    if (ioctl(g->vcpu_fd, KVM_SET_SREGS, &sregs) < 0)
+    if (ioctl(v->vcpu_fd, KVM_SET_SREGS, &sregs) < 0)
         return throw_err("Failed to set special registers");
 
     struct kvm_regs regs;
-    if (ioctl(g->vcpu_fd, KVM_GET_REGS, &regs) < 0)
+    if (ioctl(v->vcpu_fd, KVM_GET_REGS, &regs) < 0)
         return throw_err("Failed to get registers");
 
     regs.rflags = 2;
     regs.rip = 0x100000, regs.rsi = 0x10000;
-    if (ioctl(g->vcpu_fd, KVM_SET_REGS, &regs) < 0)
+    if (ioctl(v->vcpu_fd, KVM_SET_REGS, &regs) < 0)
         return throw_err("Failed to set registers");
 
     return 0;
 }
 
 #define N_ENTRIES 100
-static void vm_init_cpu_id(vm_t *g)
+static void vm_init_cpu_id(vm_t *v)
 {
     struct {
         uint32_t nent;
         uint32_t padding;
         struct kvm_cpuid_entry2 entries[N_ENTRIES];
     } kvm_cpuid = {.nent = N_ENTRIES};
-    ioctl(g->kvm_fd, KVM_GET_SUPPORTED_CPUID, &kvm_cpuid);
+    ioctl(v->kvm_fd, KVM_GET_SUPPORTED_CPUID, &kvm_cpuid);
 
     for (unsigned int i = 0; i < N_ENTRIES; i++) {
         struct kvm_cpuid_entry2 *entry = &kvm_cpuid.entries[i];
@@ -83,34 +78,34 @@ static void vm_init_cpu_id(vm_t *g)
             entry->edx = 0x4d;       /* M */
         }
     }
-    ioctl(g->vcpu_fd, KVM_SET_CPUID2, &kvm_cpuid);
+    ioctl(v->vcpu_fd, KVM_SET_CPUID2, &kvm_cpuid);
 }
 
-int vm_init(vm_t *g)
+int vm_init(vm_t *v)
 {
-    if ((g->kvm_fd = open("/dev/kvm", O_RDWR)) < 0)
+    if ((v->kvm_fd = open("/dev/kvm", O_RDWR)) < 0)
         return throw_err("Failed to open /dev/kvm");
 
-    if ((g->vm_fd = ioctl(g->kvm_fd, KVM_CREATE_VM, 0)) < 0)
+    if ((v->vm_fd = ioctl(v->kvm_fd, KVM_CREATE_VM, 0)) < 0)
         return throw_err("Failed to create vm");
 
-    if (ioctl(g->vm_fd, KVM_SET_TSS_ADDR, 0xffffd000) < 0)
+    if (ioctl(v->vm_fd, KVM_SET_TSS_ADDR, 0xffffd000) < 0)
         return throw_err("Failed to set TSS addr");
 
     __u64 map_addr = 0xffffc000;
-    if (ioctl(g->vm_fd, KVM_SET_IDENTITY_MAP_ADDR, &map_addr) < 0)
+    if (ioctl(v->vm_fd, KVM_SET_IDENTITY_MAP_ADDR, &map_addr) < 0)
         return throw_err("Failed to set identity map address");
 
-    if (ioctl(g->vm_fd, KVM_CREATE_IRQCHIP, 0) < 0)
+    if (ioctl(v->vm_fd, KVM_CREATE_IRQCHIP, 0) < 0)
         return throw_err("Failed to create IRQ chip");
 
     struct kvm_pit_config pit = {.flags = 0};
-    if (ioctl(g->vm_fd, KVM_CREATE_PIT2, &pit) < 0)
+    if (ioctl(v->vm_fd, KVM_CREATE_PIT2, &pit) < 0)
         return throw_err("Failed to create i8254 interval timer");
 
-    g->mem = mmap(NULL, RAM_SIZE, PROT_READ | PROT_WRITE,
+    v->mem = mmap(NULL, RAM_SIZE, PROT_READ | PROT_WRITE,
                   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    if (!g->mem)
+    if (!v->mem)
         return throw_err("Failed to mmap vm memory");
 
     struct kvm_userspace_memory_region region = {
@@ -118,21 +113,21 @@ int vm_init(vm_t *g)
         .flags = 0,
         .guest_phys_addr = 0,
         .memory_size = RAM_SIZE,
-        .userspace_addr = (__u64) g->mem,
+        .userspace_addr = (__u64) v->mem,
     };
-    if (ioctl(g->vm_fd, KVM_SET_USER_MEMORY_REGION, &region) < 0)
+    if (ioctl(v->vm_fd, KVM_SET_USER_MEMORY_REGION, &region) < 0)
         return throw_err("Failed to set user memory region");
 
-    if ((g->vcpu_fd = ioctl(g->vm_fd, KVM_CREATE_VCPU, 0)) < 0)
+    if ((v->vcpu_fd = ioctl(v->vm_fd, KVM_CREATE_VCPU, 0)) < 0)
         return throw_err("Failed to create vcpu");
 
-    vm_init_regs(g);
-    vm_init_cpu_id(g);
+    vm_init_regs(v);
+    vm_init_cpu_id(v);
 
     return 0;
 }
 
-int vm_load(vm_t *g, const char *image_path)
+int vm_load(vm_t *v, const char *image_path)
 {
     int fd = open(image_path, O_RDONLY);
     if (fd < 0)
@@ -145,9 +140,9 @@ int vm_load(vm_t *g, const char *image_path)
     close(fd);
 
     struct boot_params *boot =
-        (struct boot_params *) ((uint8_t *) g->mem + 0x10000);
-    void *cmdline = ((uint8_t *) g->mem) + 0x20000;
-    void *kernel = ((uint8_t *) g->mem) + 0x100000;
+        (struct boot_params *) ((uint8_t *) v->mem + 0x10000);
+    void *cmdline = ((uint8_t *) v->mem) + 0x20000;
+    void *kernel = ((uint8_t *) v->mem) + 0x100000;
 
     memset(boot, 0, sizeof(struct boot_params));
     memmove(boot, data, sizeof(struct boot_params));
@@ -168,22 +163,22 @@ int vm_load(vm_t *g, const char *image_path)
     return 0;
 }
 
-void vm_exit(vm_t *g)
+void vm_exit(vm_t *v)
 {
-    close(g->kvm_fd);
-    close(g->vm_fd);
-    close(g->vcpu_fd);
-    munmap(g->mem, RAM_SIZE);
+    close(v->kvm_fd);
+    close(v->vm_fd);
+    close(v->vcpu_fd);
+    munmap(v->mem, RAM_SIZE);
 }
 
-int vm_run(vm_t *g)
+int vm_run(vm_t *v)
 {
-    int run_size = ioctl(g->kvm_fd, KVM_GET_VCPU_MMAP_SIZE, 0);
+    int run_size = ioctl(v->kvm_fd, KVM_GET_VCPU_MMAP_SIZE, 0);
     struct kvm_run *run =
-        mmap(0, run_size, PROT_READ | PROT_WRITE, MAP_SHARED, g->vcpu_fd, 0);
+        mmap(0, run_size, PROT_READ | PROT_WRITE, MAP_SHARED, v->vcpu_fd, 0);
 
     while (1) {
-        if (ioctl(g->vcpu_fd, KVM_RUN, 0) < 0)
+        if (ioctl(v->vcpu_fd, KVM_RUN, 0) < 0)
             return throw_err("Failed to execute kvm_run");
 
         switch (run->exit_reason) {
