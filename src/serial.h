d2ee1ff2a6b794f6470a5c08bcde8c5a68cbeea7
RinHizakura <s921975628@gmail.com>
2021-09-23 17:16:02 +0800

    Emulate the basic requiremnt of UART to boot Linux
    This patch implements more emulation of the serial UART. Therefore,
    we can perform serial communications more completely, including receive
    information and interact with the command line for some BusyBox
    Unix utilities.

---
+++ src/serial.h
@@ -0,0 +1,25 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+#include <linux/kvm.h>
+#include <pthread.h>
+#include <stdint.h>
+
+#define COM1_PORT_BASE 0x03f8
+#define COM1_PORT_SIZE 8
+#define COM1_PORT_END (COM1_PORT_BASE + COM1_PORT_SIZE)
+
+typedef struct serial_dev serial_dev_t;
+
+struct serial_dev {
+    void *priv;
+    pthread_mutex_t lock;
+    pthread_t worker_tid;
+    int infd;  // file descriptor for serial input
+};
+
+void serial_init(serial_dev_t *s);
+void serial_handle(serial_dev_t *s, struct kvm_run *r);
+void serial_exit(serial_dev_t *s);
+
+#endif
---
+++ src/utils.h
@@ -0,0 +1,43 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#define container_of(ptr, type, member)               \
+    ({                                                \
+        void *__mptr = (void *) (ptr);                \
+        ((type *) (__mptr - offsetof(type, member))); \
+    })
+
+#define FIFO_LEN 64
+#define FIFO_MASK (FIFO_LEN - 1)
+
+struct fifo {
+    uint8_t data[FIFO_LEN];
+    unsigned int head;
+    unsigned int tail;
+};
+
+#define fifo_is_empty(fifo) ((fifo)->head == (fifo)->tail)
+
+#define fifo_is_full(fifo) ((fifo)->tail - (fifo)->head > FIFO_MASK)
+
+#define fifo_put(fifo, value)                               \
+    ({                                                      \
+        unsigned int __ret = !fifo_is_full(fifo);           \
+        if (__ret) {                                        \
+            (fifo)->data[(fifo)->tail & FIFO_MASK] = value; \
+            (fifo)->tail++;                                 \
+        }                                                   \
+        __ret;                                              \
+    })
+
+#define fifo_get(fifo, value)                               \
+    ({                                                      \
+        unsigned int __ret = !fifo_is_empty(fifo);          \
+        if (__ret) {                                        \
+            value = (fifo)->data[(fifo)->head & FIFO_MASK]; \
+            (fifo)->head++;                                 \
+        }                                                   \
+        __ret;                                              \
+    })
+
+#endif
